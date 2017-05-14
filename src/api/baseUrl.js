f7982a249c78eb5a5364828c1582a73989bd2144
Aleksandr Fokin <aleksandr.fokin@gmail.com>
2017-05-15 00:42:24 +0300

    Production Deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://hidden-citadel-85509.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
